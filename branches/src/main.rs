3bee599d46215cddccdd3b2e7917e4f3b29f0fdc
Ayush Soni <ayushsoni1021@gmail.com>
2025-08-10 00:04:45 +0530

    branches

---
+++ branches/src/main.rs
@@ -0,0 +1,39 @@
+// fn main() {
+//    let number = 3;
+
+//    if number < 5 {
+//     println!("the number is less than 5");
+//    }else{
+//     println!("the number is greater than 5");
+//    }
+// }
+
+
+// fn main() {
+//     let condition = true;
+
+//     let number = if condition { 5 } else { "six" };
+
+//     println!("the value of number is : {number} ");
+
+// }
+
+// fn main() {
+//     let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+
+//         if counter == 10 {
+//             break counter*2;
+//         }
+//     };
+//     println!("the result value is : {result}");
+// }
+
+fn main() {
+    for number in (1..9).rev() {
+        print!("{number} ");
+    }
+    println!("LIFTOFF!");
+}
