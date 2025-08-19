8845c701509073582c413b57bdf3c1ebcb435b0b
Ayush Soni <ayushsoni1021@gmail.com>
2025-08-19 23:44:31 +0530

    initialize ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+let mut v: Vec<i32> = vec![1, 2, 3];
+let num: &i32 = &v[2];
+v.push(4);
+println!("Third element is {}", *num);
+}
+
+// Initially, v points to an array with 3 elements on the heap. 
+// Then num is created as a reference to the third element, as seen at L1. However, 
+// the operation v.push(4) resizes v. The resize will deallocate the previous array and allocate a new, bigger array.
+//  In the process, num is left pointing to invalid memory.
+//  Therefore at L3, dereferencing *num reads invalid memory, causing undefined behavior.
