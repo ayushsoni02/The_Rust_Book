f37b475a748b811dfdff64a470ad987ee68a6085
Ayush Soni <ayushsoni1021@gmail.com>
2025-08-27 21:31:07 +0530

    update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,13 @@
 
+// fn main() {
+// let mut v: Vec<i32> = vec![1, 2, 3];
+// let num: &i32 = &v[2];
+// v.push(4);
+// println!("Third element is {}", *num);
+// }
 
 fn main() {
-let mut v: Vec<i32> = vec![1, 2, 3];
-let num: &i32 = &v[2];
-v.push(4);
-println!("Third element is {}", *num);
+    
 }
 
 
