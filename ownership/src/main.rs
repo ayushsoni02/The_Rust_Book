8e748bfaff8c2abb7ec1e27cd7ce1cbda2882b70
Ayush Soni <ayushsoni1021@gmail.com>
2025-09-09 23:40:33 +0530

    updated

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -14,5 +14,5 @@ fn main() {
 // Initially, v points to an array with 3 elements on the heap. 
 // Then num is created as a reference to the third element, as seen at L1. However, 
 // the operation v.push(4) resizes v. The resize will deallocate the previous array and allocate a new, bigger array.
-//  In the process, num is left pointing to invalid memory.
-//  Therefore at L3, dereferencing *num reads invalid memory, causing undefined behavior.
+// In the process, num is left pointing to invalid memory.
+// Therefore at L3, dereferencing *num reads invalid memory, causing undefined behavior.
